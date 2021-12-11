29ed4c8fd1ad4c616df0fa52fea5aec9062575c4
Yuanjun Zhong <zhuimengzyj@gmail.com>
2021-12-11 09:58:08 +0800

    build: fix log4js bug

--- src/main/java/com/drl/demo/DemoApplication.java
+++ src/main/java/com/drl/demo/DemoApplication.java
@@ -1,12 +1,16 @@
 package com.drl.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+    private final static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
+        logger.info("hello {}", "${java:vm}");
         SpringApplication.run(DemoApplication.class, args);
     }
 
